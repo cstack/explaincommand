423f8e6f444946d5c381179b8d2c0291e6f248d1
Connor Stack <sirconnorstack@gmail.com>
2022-11-12 16:20:54 -0500

    Fix spec

--- spec/lib/explainer_spec.rb
+++ spec/lib/explainer_spec.rb
@@ -8,7 +8,7 @@ describe Explainer do
       let(:command) { 'ls -ltr /tmp' }
       it 'explains the command' do
         expect(subject).to be_a(Explanation)
-        expect(subject.command).to eq('ls')
+        expect(subject.command_name).to eq('ls')
         expect(subject.annotations).to eq(
           [
             [
@@ -31,7 +31,7 @@ describe Explainer do
     context 'with a subcommand' do
       let(:command) { 'git add test.txt' }
       it 'identifies the manpage for the subcommand' do
-        expect(subject.command).to eq('git-add')
+        expect(subject.command_name).to eq('git-add')
       end
     end
 
