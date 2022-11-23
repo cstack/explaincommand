7e8720dfaa3e76d6d4a54e400f5d12c15ca8b78c
Connor Stack <sirconnorstack@gmail.com>
2022-11-23 18:22:11 -0500

    Break out separate UsageParser class

--- app/lib/manpage_parser.rb
+++ app/lib/manpage_parser.rb
@@ -100,7 +100,7 @@ class ManpageParser
 
   def self.extract_positional_arguments_from_paragraph(text:, command_name:)
     first_usage = extract_first_usage_from_paragraph(text:, command_name:)
-    extract_positional_arguments_from_usage_pattern(text: first_usage, command_name:)
+    UsageParser.extract_positional_arguments_from_usage_pattern(text: first_usage, command_name:)
   end
 
   def self.extract_first_usage_from_paragraph(text:, command_name:)
@@ -113,46 +113,5 @@ class ManpageParser
                              words[index_of_first_usage, index_of_second_usage + command_name.num_words]
                            end
     words_of_first_usage.join(' ')
-  end
-
-  def self.extract_positional_arguments_from_usage_pattern(text:, command_name:)
-    words = text.split
-    num_words_in_command = command_name.num_words
-    grouped_words = group_by_brackets(words)
-    positional_argument_words = grouped_words.drop(num_words_in_command).reject do |word|
-      word.gsub('[', '').start_with?('-') ||
-        word == '[OPTIONS]'
-    end
-    interpret_ellipses(positional_argument_words)
-  end
-
-  def self.group_by_brackets(words)
-    result = []
-    while words.length > 0
-      word = words.shift
-      word = "#{word} #{words.shift}" while word.count('[') > word.count(']') && words.length > 0
-      result << word
-    end
-    result
-  end
-
-  def self.interpret_ellipses(words)
-    result = []
-    words.each do |word|
-      if word == '...'
-        result.last[:repeated] = true
-      elsif word.include?('...')
-        result << {
-          name: word,
-          repeated: true
-        }
-      else
-        result << {
-          name: word,
-          repeated: false
-        }
-      end
-    end
-    result
   end
 end
---
+++ app/lib/usage_parser.rb
@@ -0,0 +1,42 @@
+class UsageParser
+  def self.extract_positional_arguments_from_usage_pattern(text:, command_name:)
+    words = text.split
+    num_words_in_command = command_name.num_words
+    grouped_words = group_by_brackets(words)
+    positional_argument_words = grouped_words.drop(num_words_in_command).reject do |word|
+      word.gsub('[', '').start_with?('-') ||
+        word == '[OPTIONS]'
+    end
+    interpret_ellipses(positional_argument_words)
+  end
+
+  def self.group_by_brackets(words)
+    result = []
+    while words.length > 0
+      word = words.shift
+      word = "#{word} #{words.shift}" while word.count('[') > word.count(']') && words.length > 0
+      result << word
+    end
+    result
+  end
+
+  def self.interpret_ellipses(words)
+    result = []
+    words.each do |word|
+      if word == '...'
+        result.last[:repeated] = true
+      elsif word.include?('...')
+        result << {
+          name: word,
+          repeated: true
+        }
+      else
+        result << {
+          name: word,
+          repeated: false
+        }
+      end
+    end
+    result
+  end
+end
