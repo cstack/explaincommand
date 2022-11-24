a91abf53d74ce0fdadacdfce18f5bf8fbb3381cd
Connor Stack <sirconnorstack@gmail.com>
2022-11-23 19:40:06 -0500

    Remove unused method

--- app/lib/manpage_parser.rb
+++ app/lib/manpage_parser.rb
@@ -78,26 +78,6 @@ class ManpageParser
     html.css('h1#NAME').first.parent.css('p').first.text.split(' - ').last
   end
 
-  def self.extract_flags(text)
-    text.match(/\s\w+\s/)
-    first_non_flag_position = Regexp.last_match&.begin(0) || text.length
-    flags_with_match_data = text.enum_for(:scan, /--?[\w-]+/).map do |flag|
-      match_data = Regexp.last_match
-      [flag, match_data]
-    end
-    flags_with_match_data_at_beginning = flags_with_match_data.select do |_flag, match_data|
-      match_data.begin(0) < first_non_flag_position
-    end
-    return nil if flags_with_match_data_at_beginning.length == 0
-
-    aliases = flags_with_match_data_at_beginning.map(&:first)
-    where_flags_end = flags_with_match_data_at_beginning.last.last.end(0)
-    text_after_flags = text[where_flags_end, text.length]
-    spacers = text_after_flags.scan(/\s+/)
-    takes_argument = spacers.length > 1 && spacers[0].length == 1 && spacers[1].length > 1
-    Flag.new(aliases:, description: text, takes_argument:)
-  end
-
   def self.extract_positional_arguments_from_paragraph(text:, command_name:)
     first_usage = extract_first_usage_from_paragraph(text:, command_name:)
     UsageParser.extract_positional_arguments_from_usage_pattern(text: first_usage, command_name:)
