cfdc6a13b9986ecd50e70c1eab8245ede028c53d
Connor Stack <sirconnorstack@gmail.com>
2022-12-09 16:38:57 -0500

    Handle when usage patterns show up in NAME section

--- app/lib/manpage_parser.rb
+++ app/lib/manpage_parser.rb
@@ -2,8 +2,9 @@ class ManpageParser
   def self.parse_html_string(html_string:, command_name:)
     html = Nokogiri::HTML(html_string)
     description = extract_description(html)
-    synopsis_text = extract_synopsis_text(html)
-    positional_arguments = extract_positional_arguments_from_paragraph(text: synopsis_text, command_name:)
+    usage_text = extract_synopsis_text(html)
+    usage_text = extract_usage_from_name_section(html) if usage_text.nil?
+    positional_arguments = extract_positional_arguments_from_paragraph(text: usage_text, command_name:)
     flags = FlagParser.extract_flags(html)
     Manpage.new(
       command_name:,
@@ -14,9 +15,18 @@ class ManpageParser
   end
 
   def self.extract_synopsis_text(html)
-    synopsis_section = html.css('h1#SYNOPSIS').first.parent
-    synopsis_section.css('h1#SYNOPSIS').remove
-    synopsis_section.text.strip.gsub(/\s+/, ' ')
+    section = html.css('h1#SYNOPSIS').first&.parent
+    return nil if section.nil?
+
+    section.css('h1').remove
+    section.text
+  end
+
+  def self.extract_usage_from_name_section(html)
+    section = html.css('h1#NAME').first&.parent
+    return nil if section.nil?
+
+    section.css('p')[1].text
   end
 
   def self.extract_description(html)
@@ -24,6 +34,7 @@ class ManpageParser
   end
 
   def self.extract_positional_arguments_from_paragraph(text:, command_name:)
+    text = text.strip.gsub(/\s/, ' ')
     first_usage = extract_first_usage_from_paragraph(text:, command_name:)
     UsageParser.extract_positional_arguments_from_usage_pattern(text: first_usage, command_name:)
   end
